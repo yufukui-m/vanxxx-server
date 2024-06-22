923b609d35ad8eec5b6326b5f7de884aa8aa24dd
dummy <dummy@localhost>
2024-06-23 02:36:24 +0900

    first template page from https://gin-gonic.com/docs/examples/html-rendering/

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
 	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/signup", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "signup.tmpl", gin.H{
+			"title": "Main website",
+		})
 	})
 
 	return r
