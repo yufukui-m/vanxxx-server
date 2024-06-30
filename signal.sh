9cf90352c4286580e451feeb23fc830492881711
dummy <dummy@localhost>
2024-06-30 13:50:15 +0900

    signal.sh unix signals testbed

---
+++ signal.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+trap 'echo "SIGINT received" && exit' INT
+trap 'echo "SIGTERM received" && exit' TERM
+
+echo "this script is running with pid $$"
+
+echo installed traps...
+trap -p
+
+echo waiting signals INT and TERM
+
+while true
+do
+  sleep 1
+done
