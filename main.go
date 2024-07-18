237d8b71cde49dc961af9bc1a62f9cbc5abb086b
dummy <dummy@localhost>
2024-07-19 06:19:13 +0900

    upload file from https://github.com/gin-gonic/examples/tree/master/upload-file/single

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
 
@@ -195,6 +196,27 @@ func setupRouter() *gin.Engine {
 	r.POST("/logout", func(c *gin.Context) {
 		expireSession(c)
 		c.String(http.StatusOK, "logged out")
+	})
+
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+	r.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Source
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+
+		filename := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, filename); err != nil {
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			return
+		}
+
+		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)
 	})
 
 	return r
