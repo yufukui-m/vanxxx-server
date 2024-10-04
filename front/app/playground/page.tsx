034f3733870a1d3d2a576f705f202ad4e75584e7
dummy <pprmz5+yufukui-m@gmail.com>
2024-10-04 09:28:51 +0900

    tailwind css

--- front/app/playground/page.tsx
+++ front/app/playground/page.tsx
@@ -1,8 +1,11 @@
 export default function Page() {
   return (
     <div>
-      <h1>Welcome to my app!</h1>
+      <h1 className="text-blue-500">I'm blue!</h1>
       <p>This is the home page.</p>
+      <div
+        className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+      />
     </div>
   )
 }
