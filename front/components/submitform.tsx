b11754deecf5c43b5610d9633e856ac43b08d4f9
dummy <pprmz5+yufukui-m@gmail.com>
2024-12-13 02:59:22 +0900

    update next and node

---
+++ front/components/submitform.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { FormEvent } from 'react';
+
+export default function SubmitForm() {
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+     
+        const formData = new FormData(event.currentTarget)
+        const response = await fetch('/api/submit', {
+          method: 'POST',
+          body: formData,
+        })
+     
+        // Handle response if necessary
+        const data = await response.json()
+        // ...
+    }
+     
+    return (
+        <form onSubmit={onSubmit}>
+          <input type="text" name="name" />
+          <button type="submit">Submit</button>
+        </form>
+    )
+}
---
+++ front/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { FormEvent } from 'react';
+import  SubmitForm  from '../../components/submitform';
+
+export default function Home() {
+  return (
+  <div>
+  <SubmitForm />
+  </div>
+  )
+}
