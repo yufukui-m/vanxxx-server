c8b0bce7c2df3e227e1581905484374bd4a8dda3
dummy <pprmz5+yufukui-m@gmail.com>
2024-12-20 07:46:21 +0900

    next public api url from env var

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env : {
-    NEXT_PUBLIC_API_URL: "http://localhost:8080",
-},
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
